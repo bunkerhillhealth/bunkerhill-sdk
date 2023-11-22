1d60877c89de938c3beb8a4565a99b543a4d0fcb
Gabe Alvarez <gabeqaz@gmail.com>
2023-11-22 13:07:46 +0100

    Make functionality and documentation consistent

---
+++ client_sdk/python/sample.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from bunkerhill_inference_api import InferenceAPIClient
+
+
+async def main():
+    async with InferenceAPIClient(
+        username="datashare-admin",
+        private_key_filename="/Users/gabealvarez/bunkerhill/bunkerhill/private_key.pem",
+        base_url="https://api.coppshillhealth.com/",
+    ) as client:
+        inference_list = await client.get_inferences(
+            model_id="e7ad8122-14cf-4bb2-b57f-075a07b51e2b",
+            patient_mrn="1",
+            segmentation_destination_dirname=".",
+        )
+        print(inference_list)
+
+
+asyncio.run(main())
