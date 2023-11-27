fcebd20224d37c655d18ca0476c656b251b54f04
Gabe Alvarez <gabeqaz@gmail.com>
2023-11-27 16:33:50 +0100

    Remove coppshill base url

--- client_sdk/javascript/src/sample.ts
+++ client_sdk/javascript/src/sample.ts
@@ -5,9 +5,8 @@ import { InferenceAPIClient } from './client';
 async function main() {
   const username = 'datashare-admin';
   const privateKeyFilename = '/Users/gabealvarez/bunkerhill/bunkerhill/private_key.pem';
-  const baseUrl = 'https://api.coppshillhealth.com/';
 
-  const client = new InferenceAPIClient(username, privateKeyFilename, undefined, baseUrl);
+  const client = new InferenceAPIClient(username, privateKeyFilename);
 
   const modelId = 'e7ad8122-14cf-4bb2-b57f-075a07b51e2b';
   const patientMrn = '1';
--- client_sdk/python/sample.py
+++ client_sdk/python/sample.py
@@ -7,7 +7,6 @@ async def main():
     async with InferenceAPIClient(
         username="datashare-admin",
         private_key_filename="private_key.pem",
-        base_url="https://api.coppshillhealth.com/",
     ) as client:
         inference_list = await client.get_inferences(
             model_id="e7ad8122-14cf-4bb2-b57f-075a07b51e2b",
